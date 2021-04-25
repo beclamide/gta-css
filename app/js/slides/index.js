cc5fb34e657f477bc4118948e600e8c885382167
John Bower <john@industrialassets.co.uk>
2021-04-25 17:05:55 +0100

    more slides

--- app/js/slides/index.js
+++ app/js/slides/index.js
@@ -1,5 +1,5 @@
 import styles from 'reveal.js/dist/reveal.css';
-import theme from 'reveal.js/dist/theme/black.css';
+import theme from 'reveal.js/dist/theme/league.css';
 import monokai from 'reveal.js/plugin/highlight/monokai.css';
 import RevealHighlight from 'reveal.js/plugin/highlight/highlight.esm'
 import Reveal from 'reveal.js';
